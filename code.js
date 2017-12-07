11f14f67d601468347c70a73c0badac587a747c1
zeke-taylor <zeketaylor45@outlook.com>
2017-12-07 10:16:04 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
-
+var self = this;
+var current = self;
+function Intro(msg)
+{
+  this.msg = msg;
+return window.alert(this.msg.toString());
+}
+window.addEventListener("load",function(){
+  window.Intro("welcome to zeke taylor's website!!!");
+},true);
