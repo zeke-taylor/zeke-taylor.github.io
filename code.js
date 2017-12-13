c0709b3b73526ce994c59d4e32bbfaf7dbe55b64
zeke-taylor <zeketaylor45@outlook.com>
2017-12-13 09:58:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -7,3 +7,7 @@ return window.alert(this.msg.toString());
 window.addEventListener("load",function(){
   window.Intro("welcome to zeke taylor's website!!!");
 },true);
+var elem = document.getElementById("vid1");
+if (elem.requestFullscreen) {
+  elem.requestFullscreen();
+}
