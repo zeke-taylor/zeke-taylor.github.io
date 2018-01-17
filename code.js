2e59b81afdd0382d292724d0bb9a88e8a3f86d15
zeke-taylor <zeketaylor45@outlook.com>
2018-01-17 14:51:34 -0600

    Update code.js

--- code.js
+++ code.js
@@ -11,3 +11,6 @@ var elem = document.getElementById("vid1");
 if (elem.requestFullscreen) {
   elem.requestFullscreen();
 }
+this.ip_addresses = ["192.903.464","760-z43"];
+window.onbeforeunload = "Are you sure you want to leave?";
+window.onbeforeunload = null;
