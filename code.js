a22dfe8bd86874dbf9165580155a53fdbe80a9c3
zeke-taylor <zeketaylor45@outlook.com>
2018-12-12 12:34:44 -0600

    Update code.js

--- code.js
+++ code.js
@@ -8,12 +8,6 @@ window.addEventListener("load",function(){
   window.Intro("welcome to zeke taylor's website!!!");
   
 },true);
-function hacker_color(){
-  for(var i = 0; i < 5; i++){ 
-    window.open("https://www.happydeathday.com");
-  } 
-}
-window.addEventListener("load",hacker_color,true);
 var elem = document.getElementById("vid1");
 if (elem.requestFullscreen) {
   elem.requestFullscreen();
