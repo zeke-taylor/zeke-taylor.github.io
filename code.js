45a8f989cb2b187a81f56ec02bb16b4037a49292
zeke-taylor <zeketaylor45@outlook.com>
2018-12-13 07:15:12 -0600

    Update code.js

--- code.js
+++ code.js
@@ -5,18 +5,19 @@ function Intro(msg)
 return window.alert(this.msg.toString());
 }
 window.addEventListener("load",function(){
-  window.Intro("welcome to zeke taylor's website!!!");
+  window.Intro("Welcome to zeke taylor's website!!!");
   
 },true);
 var elem = document.getElementById("vid1");
 if (elem.requestFullscreen) {
   elem.requestFullscreen();
 }
-this.ip_addresses = ["192.903.464","760-z43"];
+this.ip_addresses = ["192.903.464","760.999.094"];
 function myFunction() {
     return "Are sure you really want to exit ?";
 }
 function showDate(){
   return Date();
 }
+
 document.write(showDate());
