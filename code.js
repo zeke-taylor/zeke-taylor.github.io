74c037fb36829c1bb5d4ee1fc6399cedb23c9b48
zeketaylor <zeketaylor45@outlook.com>
2020-02-28 18:40:30 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,2 @@
 var date = new Date();
-document.write("<center><p class=\"lead\">&copy; " + date.getFullYear() + "</p></center>");
+document.write("<center><p class=\"lead\" style=\"color:#3f3f3f;\">&copy; " + date.getFullYear() + "</p></center>");
