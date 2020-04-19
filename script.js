51d480b29af3d6ed57cfd2d4d70508f14a65214e
Zeke Taylor <etaylor0234@gmail.com>
2020-04-19 15:49:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var date = new Date();
-document.write("<p class=\"lead\" style=\"color:#3f3f3f;\">&copy; " + date.getFullYear() + "</p>");
+document.write("<p class=\"lead text-center\" style=\"color:#efef3f;\">&copy; " + date.getFullYear() + "</p>");
 
 function getAge(DOB) {
     var today = new Date();
